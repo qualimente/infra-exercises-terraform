12b486701b9622e8870915f66a4215c7cda9a35e
Stephen Kuenzli <skuenzli@qualimente.com>
2017-12-03 19:02:20 -0700

    Create a postgres db using the Terraform community modules.

---
+++ aws/multi_tier_app/db.tf
@@ -0,0 +1,87 @@
+resource "aws_security_group" "db" {
+  name = "db"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = "5432"
+    to_port = "5432"
+    protocol = "tcp"
+    security_groups = [ "${aws_security_group.internal_web.id}"]
+  }
+}
+
+//resource "aws_db_subnet_group" "db_subnet_group" {
+//  name = "exercise-${var.name}"
+//  subnet_ids = "${data.aws_subnet_ids.default_vpc.ids}"
+//}
+
+//resource "aws_db_instance" "default" {
+//  allocated_storage    = 10
+//  storage_type         = "gp2"
+//  engine               = "postgres"
+//  engine_version       = "9.6.5"
+//  instance_class       = "db.t1.micro"
+//  name                 = "mydb"
+//  username             = "foo"
+//  password             = "bar"
+//  db_subnet_group_name = "my_database_subnet_group"
+//  parameter_group_name = "default.mysql5.6"
+//}
+
+#####
+# DB
+#####
+module "db" {
+  #source = "../../../"
+  source = "git@github.com:terraform-aws-modules/terraform-aws-rds.git?ref=v1.4.0"
+
+  identifier = "exercise"
+
+  engine            = "postgres"
+  engine_version    = "9.6.3"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+  storage_encrypted = false
+
+  # kms_key_id        = "arm:aws:kms:<region>:<accound id>:key/<kms key id>"
+  name = "exercise"
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  username = "exercise"
+
+  password = "YourPwdShouldBeLongAndSecure!"
+  port     = "5432"
+
+  vpc_security_group_ids = ["${aws_security_group.db.id}"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # disable backups to create DB faster
+  backup_retention_period = 0
+
+  tags = {
+    Owner       = "platform"
+    Environment = "exercise"
+  }
+
+  # DB subnet group
+  subnet_ids = ["${data.aws_subnet_ids.default_vpc.ids}"]
+
+  # DB parameter group
+  family = "postgres9.6"
+
+  # Snapshot name upon DB deletion
+  final_snapshot_identifier = "exercise"
+}
+
+output "db.web.address" {
+  value = "${module.db.this_db_instance_address}"
+}
+
+output "db.web.endpoint" {
+  value = "${module.db.this_db_instance_endpoint}"
+}
+
