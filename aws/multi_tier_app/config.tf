8063eb0df4f7fc3f67df9b0876a1c4365e5bf9fe
Stephen Kuenzli <skuenzli@qualimente.com>
2017-12-05 23:42:07 -0700

    Align and markup code to storyline of presentation.

--- aws/multi_tier_app/config.tf
+++ aws/multi_tier_app/config.tf
@@ -1,1 +1,26 @@
+// Configuring the AWS provider - START
 
+variable "region" {
+  description = "AWS region to use"
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+// Configuring the AWS provider - END
+
+
+// Reconfigure backend to use remote state - START
+terraform {
+  backend "s3" {
+    bucket     = "qm-training-cm-us-east-1"
+    key        = "infra/terraform/qm-sandbox/us-east-1/cm/exercise-skuenzli.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
+    dynamodb_table = "TerraformStateLock"
+  }
+}
+
+// Reconfigure backend to use remote state - END
