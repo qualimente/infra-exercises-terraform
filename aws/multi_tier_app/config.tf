07e5526a35597fb4456cb41fab1453da8afcdec9
Stephen Kuenzli <skuenzli@qualimente.com>
2018-12-17 06:39:35 -0700

    Replace value for key name with '<your name>'

--- aws/multi_tier_app/config.tf
+++ aws/multi_tier_app/config.tf
@@ -17,7 +17,7 @@ variable "region" {
 //terraform {
 //  backend "s3" {
 //    bucket     = "qm-training-cm-us-east-1"
-//    key        = "infra/terraform/qm-sandbox/us-east-1/cm/exercise-skuenzli.tfstate"
+//    key        = "infra/terraform/qm-sandbox/us-east-1/cm/exercise-<your name>.tfstate"
 //    region     = "us-east-1"
 //    encrypt    = true
 //    dynamodb_table = "TerraformStateLock"
